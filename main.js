25fe7445f1b39d5455079f843b6573d541755300
ZenBakr <99152082+ZenBakr@users.noreply.github.com>
2023-02-28 16:50:56 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 status="";
+objects=[];
 
 function preload()
 {
@@ -18,7 +19,7 @@ function modelLoaded()
 {
     console.log("Model Loaded");
     status = true;
-    objectDetector.detect(img, gotResult);
+    detection.detect(img, gotResult);
 }
 
 function gotResult(error, results)
@@ -28,19 +29,24 @@ function gotResult(error, results)
         console.log(error);
     }
     console.log(results);
+    objects=results;
 }
 function draw()
 {
     image(img,0,0,640,420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
-    fill("#0000FF");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#0000FF");
-    rect(300,90,270,320);
+
+    if(status != "")
+    {
+        for(i=0;i<objects.length;i++)
+        {
+            document.getElementById("status").innerHTML="Status : Object Detected";
+            fill("#FF0000");
+            a=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+a+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
 
